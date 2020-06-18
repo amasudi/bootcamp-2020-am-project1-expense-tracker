3b12399247a6f49530ced0da3547be0910fa0c09
amasudi <a.masudi@gmail.com>
2020-06-18 17:56:36 +0500

    removed unreachable break statements

--- src/contexts/AppReducer.js
+++ src/contexts/AppReducer.js
@@ -2,7 +2,6 @@ export const AppReducer = (state, action) => {
   switch (action.type) {
     case "ADD":
       return { transactions: [...state.transactions, action.payload] };
-      break;
     case "REMOVE":
       let newTrans = state.transactions.filter(
         (transaction) => transaction.id !== action.payload.id
@@ -10,7 +9,6 @@ export const AppReducer = (state, action) => {
       return {
         transactions: newTrans,
       };
-      break;
     default:
       return state;
   }
