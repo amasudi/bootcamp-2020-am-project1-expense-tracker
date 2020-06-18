3994e8a1300d37e498f5827538575fc18d3296a9
amasudi <a.masudi@gmail.com>
2020-06-18 17:54:01 +0500

    removing extra blocks from AppReducer

--- src/contexts/AppReducer.js
+++ src/contexts/AppReducer.js
@@ -1,19 +1,15 @@
 export const AppReducer = (state, action) => {
   switch (action.type) {
     case "ADD":
-      {
-        return { transactions: [...state.transactions, action.payload] };
-      }
+      return { transactions: [...state.transactions, action.payload] };
       break;
     case "REMOVE":
-      {
-        let newTrans = state.transactions.filter(
-          (transaction) => transaction.id !== action.payload.id
-        );
-        return {
-          transactions: newTrans,
-        };
-      }
+      let newTrans = state.transactions.filter(
+        (transaction) => transaction.id !== action.payload.id
+      );
+      return {
+        transactions: newTrans,
+      };
       break;
     default:
       return state;
